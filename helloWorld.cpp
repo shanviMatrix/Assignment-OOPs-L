5fcfdf7774627f7d3fc3b0c8a7caad25cb79d41f
Shanvi <0805shanvi@gmail.com>
2026-08-17 12:14:54 +0530

    .

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    cout << "Hello\nWorld" << endl;
+    cout << "Hello\tWorld" << endl;
+    cout << "Hello\a" << endl;
+    cout << "Hello\rWorld" << endl;
+
+    return 0;
+}
